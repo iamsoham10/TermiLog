d5bb00435a263899f805f2cb770fc0fe39ccd99d
Soham Chitale <chitalesoham106@gmail.com>
2026-02-07 23:59:46 +0530

    created welcome page

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,24 @@ const renderer = await createCliRenderer({ exitOnCtrlC: true });
 
 renderer.root.add(
   Box(
-    { alignItems: "center", justifyContent: "center", flexGrow: 1 },
+    {
+      alignItems: "center",
+      justifyContent: "center",
+      flexGrow: 1,
+      border: true,
+      borderColor: "#FFFFFF",
+      borderStyle: "rounded",
+      backgroundColor: "#262521",
+    },
     Box(
-      { justifyContent: "center", alignItems: "flex-end" },
-      ASCIIFont({ font: "tiny", text: "OpenTUI" }),
-      Text({ content: "What will you build?", attributes: TextAttributes.DIM }),
+      { justifyContent: "center", alignItems: "center", gap: 2 },
+      ASCIIFont({ font: "tiny", text: "TermiLog" }),
+      Text({
+        justifyContent: "center",
+        alignItems: "center",
+        content: "Write your thoughts in 🐚!",
+        attributes: TextAttributes.ITALIC,
+      }),
     ),
   ),
 );
