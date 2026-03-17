41b3ff48f6fd4bddb340fcb8cfe328d50aeadc52
Soham Chitale <chitalesoham106@gmail.com>
2026-03-17 18:26:22 +0530

    feat: added dynamic placeholders

--- src/components/editor.ts
+++ src/components/editor.ts
@@ -4,12 +4,27 @@ import {
   type RenderContext,
 } from "@opentui/core";
 
+const placeholders = [
+  "How was your day?...",
+  "What’s on your mind right now?",
+  "Take a moment… what are you feeling?",
+  "What stood out today?",
+  "Let it out…",
+  "What are you carrying today?",
+  "Write whatever comes to mind.",
+  "What did today teach you?",
+  "Pause. Reflect. Write.",
+];
+
+const currentPlaceholder =
+  placeholders[Math.floor(Math.random() * placeholders.length)];
+
 export function editorComponent(renderer: RenderContext) {
   const textEditor = new TextareaRenderable(renderer, {
     id: "editor-container",
     width: "100%",
     height: "100%",
-    placeholder: "How was your day?...",
+    placeholder: currentPlaceholder,
     cursorColor: "#00FF88",
     textColor: "#e6e6e6",
   });
