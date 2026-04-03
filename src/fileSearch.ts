249692f75b03a6bf21ed0fb12fa90d13ec313346
Soham Chitale <chitalesoham106@gmail.com>
2026-04-03 23:59:22 +0530

    feat: wrote file search function

--- src/fileSearch.ts
+++ src/fileSearch.ts
@@ -3,20 +3,31 @@ import { existsSync, readFileSync } from "node:fs";
 import { homedir } from "node:os";
 import type { JournalsIndex } from "./types/journal";
 
-const readJournalNamesFromIndex = () => {
-  console.log("called");
+const readJournalNamesFromIndex = (): string[] => {
   const TERMILOG_DIR = join(homedir(), ".termilog");
   const JOURNAL_INDEX_PATH = join(TERMILOG_DIR, "journals.json");
   try {
     if (existsSync(JOURNAL_INDEX_PATH)) {
       const journalMetadata = readFileSync(JOURNAL_INDEX_PATH, "utf-8");
-      const journalFileNames: JournalsIndex[] = JSON.parse(journalMetadata);
-      console.log(journalFileNames);
+      const journalIndexData = JSON.parse(journalMetadata) as JournalsIndex;
+      const journalFileNames = journalIndexData.journals.map(
+        (fileItem) => fileItem.title,
+      );
+      return journalFileNames;
     }
-  } catch {}
+  } catch {
+    console.error("Index file parsing failed");
+    return [];
+  }
+  return [];
 };
 
-// export const fileSearch = (fileName: string): string[] => {
-//   return [];
-// };
-readJournalNamesFromIndex();
+export const fileSearch = (fileName: string): string[] => {
+  const journalNames = readJournalNamesFromIndex();
+  console.log(journalNames);
+  const results = journalNames.filter((files) => files.includes(fileName));
+  console.log(results);
+  return results;
+};
+
+fileSearch("da");
