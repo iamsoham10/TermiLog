4a7f46a277ce36cf4ba5db444b4a321c1bf2f3c7
Soham Chitale <chitalesoham106@gmail.com>
2026-04-04 07:49:48 +0530

    feat: added simple case-insensitive search in file search

--- src/fileSearch.ts
+++ src/fileSearch.ts
@@ -2,6 +2,8 @@ import { readJournalNamesFromIndex } from "./journalStorage";
 
 export const fileSearch = (fileName: string): string[] => {
   const journalNames = readJournalNamesFromIndex();
-  const results = journalNames.filter((files) => files.includes(fileName));
+  const results = journalNames.filter((files) =>
+    files.toLowerCase().includes(fileName.toLowerCase()),
+  );
   return results;
 };
